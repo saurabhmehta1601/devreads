33822fbabe221e6ad27f88e197ac001e184691de
saurabh <saurabhmehta1601@gmail.com>
2021-11-26 14:03:15 +0530

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import {gql, useQuery} from "@apollo/client"
 import React from "react"
 import Section from "../components/Section"
 import Card from "../components/Card"
-import { Spin , Alert,Row,Col ,Typography} from "antd"
+import { Spin , Alert ,Typography} from "antd"
 import { Carousel } from '@trendyol-js/react-carousel';
+import {useMediaQuery} from "react-responsive"
+import {MAX_WIDTH_MOBILE} from "../utils/constants"
 
 const query = gql`
   query allDevroutes {
@@ -22,6 +24,8 @@ const query = gql`
 
 export default function Home() {
   const {data,loading,error} =  useQuery(query)
+  const isMobile = useMediaQuery({ query: `(max-width:${MAX_WIDTH_MOBILE})` })
+  console.warn("is mobile is ",isMobile)
   return (
   <>
       {data && data.devroutes.map(devroute => <>
@@ -30,9 +34,9 @@ export default function Home() {
             {devroute.name  }
           </Typography.Title>
         </Section>
-        <Carousel show={2} swiping={true} useArrowKeys={true} infinite={false}>
+        <Carousel show={ isMobile ? 1 : 3} swiping={true} useArrowKeys={true} infinite={false}>
           { devroute.coursesByDevroute.map(course => {
-            return  <Card course={course} /> 
+            return  <Card key={course.id} course={course} /> 
             }) }
         </Carousel>
       </>)
---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+export const MIN_WIDTH_DESKTOP="1024px"
+
+export const MIN_WIDTH_TABLET="768px"
+
+export const MIN_WIDTH_MOBILE="640px"
+
+export const MAX_WIDTH_TABLET="1023px"
+
+export const MAX_WIDTH_MOBILE="767px"
+
+
