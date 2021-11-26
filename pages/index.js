f1f8bfbaae3c53552c348a94e8cd5be8bebda6de
saurabh <saurabhmehta1601@gmail.com>
2021-11-26 14:11:29 +0530

    render diff number of slides based on viewport size

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Card from "../components/Card"
 import { Spin , Alert ,Typography} from "antd"
 import { Carousel } from '@trendyol-js/react-carousel';
 import {useMediaQuery} from "react-responsive"
-import {MAX_WIDTH_MOBILE} from "../utils/constants"
+import {MAX_WIDTH_MOBILE, MAX_WIDTH_TABLET} from "../utils/constants"
 
 const query = gql`
   query allDevroutes {
@@ -25,22 +25,23 @@ const query = gql`
 export default function Home() {
   const {data,loading,error} =  useQuery(query)
   const isMobile = useMediaQuery({ query: `(max-width:${MAX_WIDTH_MOBILE})` })
+  const isTablet= useMediaQuery({ query: `(max-width:${MAX_WIDTH_TABLET})` })
   console.warn("is mobile is ",isMobile)
-  return (
-  <>
-      {data && data.devroutes.map(devroute => <>
-        <Section>
+  return ( <>
+      {data && data.devroutes.map((devroute,idx )=> (<>
+        <Section key={idx}>
           <Typography.Title level={3}>
             {devroute.name  }
           </Typography.Title>
         </Section>
-        <Carousel show={ isMobile ? 1 : 3} swiping={true} useArrowKeys={true} infinite={false}>
+        <Carousel show={ isMobile ? 1 : isTablet ? 2: 3} swiping={true} useArrowKeys={true} infinite={false}>
           { devroute.coursesByDevroute.map(course => {
             return  <Card key={course.id} course={course} /> 
             }) }
         </Carousel>
       </>)
-        }
+      )
+    }
 
     {loading && ( <Spin size="large"  />)}
 
