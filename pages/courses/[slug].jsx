ac19f6f2a653cb026ad886fd41f6f2029d881868
saurabh <saurabhmehta1601@gmail.com>
2021-11-29 18:11:33 +0530

    add css file to course page

--- pages/courses/[slug].jsx
+++ pages/courses/[slug].jsx
@@ -7,6 +7,7 @@ import { courseFilePaths,COURSES_PATH } from '../../utils/mdxUtils'
 import Link from "next/link"
 import Section from "../../components/Section"
 import { Typography } from 'antd'
+import  Head from  "next/head"
 
 const Title = ({children}) => {
   return <Typography.Title  style={{textAlign:'center',textDecoration:'underline'}} level={1}>{children}</Typography.Title>
@@ -20,6 +21,9 @@ const components = {
 
 export default function PostPage({ source, frontMatter }) {
   return (<>
+    <Head>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css"></link>
+    </Head>
       <main>
         <MDXRemote {...source} components={components} />
       </main>
