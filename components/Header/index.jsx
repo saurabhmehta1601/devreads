64b7fc41ea2d670f37c4478892171155c1e68f35
Saurabh Mehta <saurabhmehta1601@gmail.com>
2021-12-20 11:07:40 +0530

    :sparkles: feat: add button to play music while browsing
    ISSUES CLOSED: #8

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,16 +1,43 @@
-import  React  from  "react"
-import { Typography } from 'antd';
-import styles from "./styles.module.css"
-import  Link from "next/link"
+import React, { useRef, useState } from "react";
+import { Typography } from "antd";
+import styles from "./styles.module.css";
+import Link from "next/link";
+import { FaMusic } from "react-icons/fa";
 
+import { RiMusic2Fill, RiMusic2Line } from "react-icons/ri";
 
-export default function  Header (){
-  return (   
+export default function Header() {
+  const audioRef = useRef();
+  const [playMusic, setPlayMusic] = useState(false);
+  const handleClick = () => {
+    if (audioRef.current) {
+      if (!playMusic) audioRef.current.play();
+      else audioRef.current.pause();
+      setPlayMusic(!playMusic);
+    }
+  };
+
+  return (
     <header className={styles.header}>
-        <Typography.Title level={1} >
-          <div className={styles.title}>
-            <Link href="/"> DevReads </Link>
-          </div>
-        </Typography.Title>  
+      <Typography.Title level={1} className={styles.title}>
+        <Link href="/"> DevReads </Link>
+        <audio
+          ref={audioRef}
+          src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+        />
+        {playMusic ? (
+          <RiMusic2Fill
+            onClick={handleClick}
+            className={`${styles.musicIcon} ${styles.musicIconActive}`}
+          />
+        ) : (
+          <RiMusic2Line
+            onClick={handleClick}
+            className={`${styles.musicIcon} 
+            }`}
+          />
+        )}
+      </Typography.Title>
     </header>
-)}
+  );
+}
