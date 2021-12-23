3cf3a1dffe89f12d0149c85447064b85782c83c1
Saurabh Mehta <saurabhmehta1601@gmail.com>
2021-12-23 21:12:08 +0530

    :lipstick: style: change next img layout property to responsive
    ISSUES CLOSED: #10

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,30 +1,37 @@
-import { Card } from 'antd';
-import  Image from  "next/image"
-import styles from  "./styles.module.css"
+import { Card } from "antd";
+import Image from "next/image";
+import styles from "./styles.module.css";
 
-export default function CourseCard({onClick,course: {name,description,thumb_url} }){
-const { Meta } = Card;
+export default function CourseCard({
+  onClick,
+  course: { name, description, thumb_url },
+}) {
+  const { Meta } = Card;
 
+  const transformName = (name) => name.split("-").join(" ").toUpperCase();
 
-  const transformName = (name) => name.split("-").join(" ").toUpperCase() 
-
-  return  ( <Card
-    style={{ width: 300,boxShadow: '0 0 16px 4px rgba(0,0,0,0.4)' }}
-    className={styles.card}
-    onClick={onClick}
-    cover={
-      <Image
-        alt={name}
-        width={300}
-        height={250}
-        src={thumb_url}
+  return (
+    <Card
+      style={{
+        width: 300,
+        boxShadow: "0 0 16px 4px rgba(0,0,0,0.4)",
+      }}
+      className={styles.card}
+      onClick={onClick}
+      cover={
+        <Image
+          alt={name}
+          width={300}
+          height={250}
+          layout="responsive"
+          src={thumb_url}
+        />
+      }
+    >
+      <Meta
+        title={name ? transformName(name) : name}
+        description={description}
       />
-    }
-  >
-    <Meta
-      title={name ? transformName(name) : name}
-      description={description}
-    />
-  </Card>
-  )
+    </Card>
+  );
 }
