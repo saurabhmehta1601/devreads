362a1d19a39ec81a790a5567a3f793638afe4b80
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-04 20:51:51 +0530

    :sparkles: feat: setup @reduxjs/toolkit for project
    ISSUES CLOSED: #13

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,25 @@
 // react multi carousel styles
 import "react-multi-carousel/lib/styles.css";
-// ant design styles 
-import 'antd/dist/antd.css'
-import '../styles/globals.css'
-import { ApolloProvider } from '@apollo/client'
-import client from "../client"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+// ant design styles
+import "antd/dist/antd.css";
+import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "../client";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <ApolloProvider client={client}>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
+  return (
+    <ApolloProvider client={client}>
+      <ReduxProvider store={store}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ReduxProvider>
     </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from "./features/SidebarSlice";
+
+export const store = configureStore({
+  reducer: { sidebarReducer },
+});
