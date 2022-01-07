b8cfc799565bc09a46100148d35c4f4ee608f342
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-07 16:01:43 +0530

    :sparkles: feat: add breakpoints for all courses in front matter

--- components/Section/Heading/index.jsx
+++ components/Section/Heading/index.jsx
@@ -1,11 +1,11 @@
-import styled from 'styled-components'
-const H2 = ({children}) => <Heading>{children}</Heading>
+import styled from "styled-components";
+const H2 = ({ children }) => <Heading>{children}</Heading>;
 
 const Heading = styled.h2`
   background-color: var(--clr-bg-dark);
-  color: var(--clr-bg-light) ;
-  padding: 0 .5em ;
+  color: var(--clr-bg-light);
+  padding: 0.15em 0.8em;
   border-radius: 4px;
-`
+`;
 
-export default H2
+export default H2;
