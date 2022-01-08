97c5f8a8982d9c689c4ea80756f1dcec69ef2bf8
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-08 13:00:15 +0530

    :sparkles: feat: add Output component

---
+++ components/Output/index.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+export default function Output(props){
+  return <code {...props} className={styles.code}>
+    {props.children}
+    </code>
+}
--- pages/courses/[slug].jsx
+++ pages/courses/[slug].jsx
@@ -5,13 +5,15 @@ import { MDXRemote } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import { courseFilePaths, COURSES_PATH } from "../../utils/mdxUtils";
 import Link from "next/link";
-import {Section,Title,Heading,Sidebar,SidebarTriggerButton} from "../../components/exports"
+import {Section,Title,Heading,Sidebar,SidebarTriggerButton, Output, Command} from "../../components/exports"
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const components = {
   Link,
   Section,
   h2: Heading,
+  Output,
+  Command
 };
 
 export default function PostPage({ source, frontMatter }) {
