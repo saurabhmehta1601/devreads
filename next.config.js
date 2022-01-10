8178046a53b471d0bef9e30127d5614b0ad3d23e
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-10 16:02:17 +0530

    :sparkles: feat: add src folder alias to reduce long relative imports

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
   pageExtensions: ["js", "jsx", "md", "mdx"],
+  webpack: function (config) {
+    config.resolve.alias = {
+      src: path.join(__dirname, "src"),
+      ...config.resolve.alias,
+    };
+    return config;
+  },
 };
 
 const withMDX = require("@next/mdx")({
