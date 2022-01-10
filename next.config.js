a40e4d90556ab30c7fe346d1d43cd99bd49834e1
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-10 15:49:43 +0530

    :sparkles: feat: setup @next/mdx and @mdx-js/loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com']
-  }
-}
+    domains: ["res.cloudinary.com"],
+  },
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+};
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+module.exports = withMDX(nextConfig);
